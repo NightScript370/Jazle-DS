3e93add1b0958bef8f714c8d8c532dcb4380209c
NightScript <maorninja@outlook.com>
2022-04-20 01:21:46 -0500

    Directly use source code

--- generate_words.ts
+++ generate_words.ts
@@ -1,10 +1,10 @@
-import {WORDS} from "./source-from-sourcemaps/sources-gen/constants/wordlist.ts"
-import {VALID_GUESSES} from "./source-from-sourcemaps/sources-gen/constants/validGuesses.ts"
+import {WORDS} from "https://raw.githubusercontent.com/oradwell/jazle/main/src/constants/wordlist.ts"
+import {VALID_GUESSES} from "https://raw.githubusercontent.com/oradwell/jazle/main/src/constants/validGuesses.ts"
 
 const fileContent = `#include "words.hpp"
 
-std::array<const char16_t *, ${WORDS.length}> choices = {${WORDS.map(word => `u"${word}"`).join(', ')}};
-std::array<const char16_t *, ${VALID_GUESSES.length}> guesses = {${VALID_GUESSES.map(word => `u"${word}"`).join(', ')}};`;
+std::array<const char16_t *, ${WORDS.length}> choices = {${(WORDS as string[]).map(word => `u"${word}"`).join(', ')}};
+std::array<const char16_t *, ${VALID_GUESSES.length}> guesses = {${(VALID_GUESSES as string[]).map(word => `u"${word}"`).join(', ')}};`;
 
 
 await Deno.writeTextFile('WordleDS/source/words.cpp', fileContent)
