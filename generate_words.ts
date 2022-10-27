c185d7cc7a0946a78576567e4acb3ecacdfe1f84
NightScript <18664762+NightScript370@users.noreply.github.com>
2022-10-27 19:53:37 -0400

    Accomodate latest mod support
    Probably going to need a new script

--- generate_words.ts
+++ generate_words.ts
@@ -1,27 +1,14 @@
 import {WORDS} from "https://raw.githubusercontent.com/oradwell/jazle/main/src/constants/wordlist.ts"
 import {VALID_GUESSES} from "https://raw.githubusercontent.com/oradwell/jazle/main/src/constants/validGuesses.ts"
 
+import defaultModJSON from "https://raw.githubusercontent.com/Epicpkmn11/WordleDS/mods/examples/No%20spoilers/mod.json" assert { type: "json" }
+
 const correctWords = [...new Set(WORDS)].map(word => `u"${word.toUpperCase()}"`);
 const validWords = [...new Set(VALID_GUESSES)].map(word => `u"${word.toUpperCase()}"`);
 
-const wordsSource = `#include "words.hpp"
+const modJSON = Object.assign({}, defaultModJSON);
+modJSON.shareName = "Jazle DS";
+modJSON.creditStr = "Jazle DS\nby oliverradwell"
+modJSON.words = {guesses:validWords, choices:correctWords}
 
-std::array<const char16_t *, ${correctWords.length}> choices = {${correctWords.join(', ')}};
-std::array<const char16_t *, ${validWords.length}> guesses = {${validWords.join(', ')}};`;
-await Deno.writeTextFile('WordleDS/source/words.cpp', wordsSource);
-
-const wordsHeader = `#ifndef WORDS_HPP
-#define WORDS_HPP
-
-#include <array>
-
-extern std::array<const char16_t *, ${correctWords.length}> choices;
-extern std::array<const char16_t *, ${validWords.length}> guesses;
-
-#endif // WORDS_HPP`;
-await Deno.writeTextFile('WordleDS/include/words.hpp', wordsHeader);
-
-await Deno.copyFile('./bgTop.png', 'WordleDS/gfx/bgTop.png')
-await Deno.copyFile('./howtoTop.png', 'WordleDS/gfx/howtoTop.png')
-
-await Deno.copyFile('./defines.hpp', 'WordleDS/include/defines.hpp')
+await Deno.writeTextFile("./mod.json", JSON.stringify(modJSON));
