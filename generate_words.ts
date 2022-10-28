aa080dc9a08b45456b40a19c25519b15426b1d9c
NightScript <18664762+NightScript370@users.noreply.github.com>
2022-10-27 20:13:05 -0400

    Also copy wordle offset and strings
    From source, rather than defines.hpp

--- generate_words.ts
+++ generate_words.ts
@@ -1,6 +1,8 @@
 import {WORDS} from "https://raw.githubusercontent.com/oradwell/jazle/main/src/constants/wordlist.ts"
 import {VALID_GUESSES} from "https://raw.githubusercontent.com/oradwell/jazle/main/src/constants/validGuesses.ts"
 
+import * as STRINGS from "https://raw.githubusercontent.com/oradwell/jazle/main/src/constants/strings.ts"
+
 import defaultModJSON from "https://raw.githubusercontent.com/Epicpkmn11/WordleDS/mods/examples/No%20spoilers/mod.json" assert { type: "json" }
 
 const correctWords = [...new Set(WORDS)].map(word => `u"${word.toUpperCase()}"`);
@@ -9,6 +11,15 @@ const validWords = [...new Set(VALID_GUESSES)].map(word => `u"${word.toUpperCase
 const modJSON = Object.assign({}, defaultModJSON);
 modJSON.shareName = "Jazle DS";
 modJSON.creditStr = "Jazle DS\nby oliverradwell"
+
+modJSON.notWordMessage = STRINGS.WORD_NOT_FOUND_MESSAGE
+modJSON.guessMustContainX = STRING.NOT_CONTAINED_MESSAGE("%s")
+modJSON.lossMessage = STRING.CORRECT_WORD_MESSAGE("%s")
+modJSON.tooShortMessage = STRING.NOT_ENOUGH_LETTERS_MESSAGE
+modJSON.nthMustBeX = STRING.WRONG_SPOT_MESSAGE("%s", "%d%") //TypeError, I know
+modJSON.victoryMessages = STRING.WIN_MESSAGES
+
+modJSON.wordleOffset = 235;
 modJSON.words = {guesses:validWords, choices:correctWords}
 
 await Deno.writeTextFile("./mod.json", JSON.stringify(modJSON));
